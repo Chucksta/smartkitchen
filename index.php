600a5e4a4739eb23caf25210a9b6332205926785
Charlie McMichael <charliemcmichael@gmail.com>
2013-01-29 17:32:39 -0500

    rename files and add new ones

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <!-- user input for api for item query -->
+        
+        <form name="search" action="search.php" method="post">
+            Search Item:<input type="text" name="item"> </br>
+            <!-- working on item first Search Ingredients (CSV):<input type="text" name="ingrd"> </br> -->
+            <input type="submit" value="submit">
+        </form>
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
