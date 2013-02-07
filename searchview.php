f11948e595074ec8051b84d1ef3ed81a64c06f28
Charlie McMichael <charliemcmichael@gmail.com>
2013-02-07 11:30:28 -0500

    added web template

--- searchview.php
+++ searchview.php
@@ -1,22 +1,30 @@
-<?php
 
-$connect = mysql_connect('localhost', 'root', 'root') or die(mysql_error());
-mysql_select_db('smartkitchen') or die(mysql_error());
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <input type="button" value="Return to Homepage" onClick="location.href='index.php'">
+        <?php
+        $connect = mysql_connect('localhost', 'root', 'root') or die(mysql_error());
+        mysql_select_db('smartkitchen') or die(mysql_error());
 
-$sqlS = "SELECT * FROM recipies";
-$resultS = mysql_query($sqlS);
+        $sqlS = "SELECT * FROM recipies";
+        $resultS = mysql_query($sqlS);
 
-while ($rowS = mysql_fetch_assoc($resultS)) {
-    echo '<center>';
-    echo '<hr>';
-    $resultS = mysql_query("SELECT * FROM recipies ORDER BY title DESC");
-    while ($rowS = mysql_fetch_assoc($resultS)) {
-        echo "<a href='" . $rowS['href'] . "'>" . $rowS['title'] . "</a></br>";
-        echo $rowS['ingredients'] . "</br>";
-        echo "<a href='" . $rowS['href'] . "'><img src='" . $rowS['thumbnail'] . "'></a></br>";
-        echo "<hr>";
-
-    }
-    echo '</center>';
-}
-?>
+        while ($rowS = mysql_fetch_assoc($resultS)) {
+            echo '<center>';
+            echo '<hr>';
+            $resultS = mysql_query("SELECT * FROM recipies ORDER BY title DESC");
+            while ($rowS = mysql_fetch_assoc($resultS)) {
+                echo "<a href='" . $rowS['href'] . "'>" . $rowS['title'] . "</a></br>";
+                echo $rowS['ingredients'] . "</br>";
+                echo "<a href='" . $rowS['href'] . "'><img src='" . $rowS['thumbnail'] . "'></a></br>";
+                echo "<hr>";
+            }
+            echo '</center>';
+        }
+        ?>
+    </body>
+</html>
--- spider.php
+++ spider.php
@@ -3,13 +3,16 @@
 $connect = mysql_connect('localhost', 'root', 'root') or die(mysql_error());
 mysql_select_db('smartkitchen') or die(mysql_error());
 
-$api = "http://www.recipepuppy.com/api/?q=pancake";
-$regq = "q=";
+$api = "http://www.recipepuppy.com/api/?";
+$regQ = "q=";
+$reqI = "i=";
 $sItem = $_POST["item"];
+$url = $api . $regQ . $sItem ;
 
-
-$contents = file_get_contents("$api . $regq . $sItem");
+$contents = file_get_contents("$url");
 $json = json_decode($contents);
+
+$truncateR = mysql_query('TRUNCATE TABLE `recipies`');
 
 foreach ($json->results as $result) {
 
@@ -17,7 +20,7 @@ foreach ($json->results as $result) {
         '$result->ingredients', '$result->thumbnail')");
 
 }
-
+mysql_close($connect);
+echo "seached for: " . $url;
 echo "<meta http-equiv='refresh' content='1,searchview.php'/>";
 ?>
-
