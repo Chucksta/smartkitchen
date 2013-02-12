9ab4973128b1f3279aaa86cf7bdc39e3a2a89065
Nate <theodomizer@aol.com>
2013-02-11 20:05:11 -0500

    small template additions

---
+++ webtemplate/spider.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+<?php
+
+$connect = mysql_connect('localhost', 'root', '') or die(mysql_error());
+mysql_select_db('smartkitchen') or die(mysql_error());
+
+$api = "http://www.recipepuppy.com/api/?";
+$regQ = "q=";
+$reqI = "i=";
+$sItem = $_POST["item"];
+$url = $api . $regQ . $sItem ;
+
+$contents = file_get_contents("$url");
+$json = json_decode($contents);
+
+$truncateR = mysql_query('TRUNCATE TABLE `recipies`');
+
+foreach ($json->results as $result) {
+
+    mysql_query("INSERT INTO recipies (title,href,ingredients, thumbnail) VALUE ('$result->title', '$result->href',
+        '$result->ingredients', '$result->thumbnail')");
+
+}
+mysql_close($connect);
+echo "seached for: " . $url;
+echo "<meta http-equiv='refresh' content='1,searchview.php'/>";
+?>
+    </body>
+</html>
